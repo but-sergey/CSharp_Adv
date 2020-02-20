907a801d10ba30420dd42d99d110ecf7fe1d9c15
but-sergey <but-sergey@bk.ru>
2020-02-20 14:46:27 +0300

    Рефакторинг

---
+++ AsteroidGame/TestConsole/Gamer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+
+    class Gamer
+    {
+        private string _Name;
+        private DateTime _DayOfBirth;
+
+        public string Name
+        {
+            get
+            {
+                return _Name ?? String.Empty;
+            }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(value));
+                _Name = value;
+            }
+        }
+
+        public Gamer(string Name, DateTime DayOfBirth)
+        {
+            _Name = Name;
+            _DayOfBirth = DayOfBirth;
+        }
+
+        //public void SetName(string value)
+        //{
+        //    _Name = value;
+        //}
+
+        //public string GetName()
+        //{
+        //    return _Name;
+        //}
+
+        internal int GetNameLength()
+        {
+            return Name.Length;
+        }
+
+        public void SayYouName()
+        {
+            Console.WriteLine("My name is {0} - {1:dd:MM:yyyy HH:mm:ss}", _Name, _DayOfBirth);
+        }
+    }
+}
--- AsteroidGame/TestConsole/Program.cs
+++ AsteroidGame/TestConsole/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace TestConsole
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            Gamer gamer = new Gamer("Sergey", new DateTime(1985, 06, 05, 11, 30, 00));
+
+            Gamer[] gamers = new Gamer[100];
+            for(var i = 0; i < gamers.Length; i++)
+            {
+                var g = new Gamer($"Gamer {i + 1}", DateTime.Now.Subtract(TimeSpan.FromDays(365 * (i + 18))));
+                gamers[i] = g;
+            }
+            
+            gamer.SayYouName();
+
+            Console.WriteLine();
+
+            foreach (var g in gamers)
+                g.SayYouName();
+
+            Console.WriteLine();
+
+            //gamer.SetName("2222");
+            //Console.WriteLine("Gamer's name is {0}", gamer.GetName());
+
+            gamer.Name = "123";
+
+            Console.WriteLine("Gamer's name is {0}", gamer.Name);
+
+            Console.ReadLine();
         }
     }
+
 }
