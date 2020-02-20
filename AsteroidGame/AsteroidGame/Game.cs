1732a4498a57b34b1f9691457ed1d29ee63e6633
but-sergey <but-sergey@bk.ru>
2020-02-20 21:16:08 +0300

    Пример рисования графики

--- AsteroidGame/AsteroidGame/Game.cs
+++ AsteroidGame/AsteroidGame/Game.cs
@@ -30,5 +30,16 @@ namespace AsteroidGame
             Graphics g = form.CreateGraphics();
             __Buffer = __Context.Allocate(g, new Rectangle(0, 0, Width, Height));
         }
+
+        public static void Draw()
+        {
+            var g = __Buffer.Graphics;
+            g.Clear(Color.Black);
+
+            g.DrawRectangle(Pens.White, new Rectangle(50, 50, 200, 200));
+            g.FillEllipse(Brushes.Red, new Rectangle(100, 50, 70, 120));
+
+            __Buffer.Render();
+        }
     }
 }
