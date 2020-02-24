d51e17cd0cffafd19e1ec3319aa432000e461215
but-sergey <but-sergey@bk.ru>
2020-02-24 17:30:52 +0300

    Loggers

--- AsteroidGame/TestConsole/Program.cs
+++ AsteroidGame/TestConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,27 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+            //Logger logger = new ListLogger();
+            //Logger logger = new FileLogger("program.log");
+            //Logger logger = new VisualStudioOutputLogger();
+            Logger logger = new TraceLogger();
 
+            Trace.Listeners.Add(new TextWriterTraceListener("trace.logger"));
+
+            logger.LogInformation("Start program");
+
+            for(int i = 0; i < 10; i++)
+                logger.LogInformation($"Do some work {i + 1}");
+
+            logger.LogWarning("Application work out");
+
+            //var log_messages = ((ListLogger)logger).Messages;
+
+            Trace.Flush();
 
             Console.ReadLine();
         }
     }
 }
+
+
