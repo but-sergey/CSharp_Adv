f7e413bea2273664eb18a64f9611c64a9575647f
but-sergey <but-sergey@bk.ru>
2020-02-25 21:12:18 +0300

    Рефакторинг

--- AsteroidGame/Workers/Classes/BaseWorker.cs
+++ AsteroidGame/Workers/Classes/BaseWorker.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Workers.Classes
 {
-    public abstract class BaseWorker
+    public abstract class BaseWorker : IComparable
     {
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
@@ -18,5 +18,23 @@ namespace Workers.Classes
         }
 
         public abstract double CalcSalary();
+
+        public int CompareTo(object obj)
+        {
+            if (obj is BaseWorker)
+            {
+                if (CalcSalary() == ((BaseWorker)obj).CalcSalary())
+                    return 0;
+                else if (CalcSalary() > ((BaseWorker)obj).CalcSalary())
+                    return +1;
+                else
+                    return -1;
+            }
+            else if (obj is null)
+                throw new ArgumentNullException("Попытка сравнения с пустотой");
+            else
+                throw new ArgumentException("Попытка сравнения с " + obj.GetType().Name, nameof(obj));
+        }
+
     }
 }
--- AsteroidGame/Workers/Classes/HoursRateWorker.cs
+++ AsteroidGame/Workers/Classes/HoursRateWorker.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Workers.Classes
 {
-    public class HoursRateWorker : BaseWorker, IComparable
+    public class HoursRateWorker : BaseWorker
     {
         public HoursRateWorker(string name, DateTime birth_date, double hours_rate) 
             : base(name, birth_date)
@@ -21,21 +21,5 @@ namespace Workers.Classes
             return 20.8 * 8 * HoursRate;
         }
 
-        public int CompareTo(object obj)
-        {
-            if (obj is BaseWorker)
-            {
-                if (CalcSalary() == ((HoursRateWorker)obj).CalcSalary())
-                    return 0;
-                else if (CalcSalary() > ((HoursRateWorker)obj).CalcSalary())
-                    return +1;
-                else
-                    return -1;
-            }
-            else if (obj is null)
-                throw new ArgumentNullException("Попытка сравнения с пустотой");
-            else
-                throw new ArgumentException("Попытка сравнения с " + obj.GetType().Name, nameof(obj));
-        }
     }
 }
