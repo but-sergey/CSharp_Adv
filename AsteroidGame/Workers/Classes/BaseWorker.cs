f69765a3716136ace26b48a2c7bc39a56e6b0686
but-sergey <but-sergey@bk.ru>
2020-02-25 21:09:21 +0300

    Добавлены классы работников, массив работников, интерфейс IComparable. Массив работников отсортирован по зарплате

---
+++ AsteroidGame/Workers/Classes/BaseWorker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workers.Classes
+{
+    public abstract class BaseWorker
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        protected BaseWorker(string name, DateTime birthDate)
+        {
+            Name = name;
+            BirthDate = birthDate;
+        }
+
+        public abstract double CalcSalary();
+    }
+}
---
+++ AsteroidGame/Workers/Classes/HoursRateWorker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workers.Classes
+{
+    public class HoursRateWorker : BaseWorker, IComparable
+    {
+        public HoursRateWorker(string name, DateTime birth_date, double hours_rate) 
+            : base(name, birth_date)
+        {
+            HoursRate = hours_rate;
+        }
+
+        public double HoursRate { get; set; }
+
+        public override double CalcSalary()
+        {
+            return 20.8 * 8 * HoursRate;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is BaseWorker)
+            {
+                if (CalcSalary() == ((HoursRateWorker)obj).CalcSalary())
+                    return 0;
+                else if (CalcSalary() > ((HoursRateWorker)obj).CalcSalary())
+                    return +1;
+                else
+                    return -1;
+            }
+            else if (obj is null)
+                throw new ArgumentNullException("Попытка сравнения с пустотой");
+            else
+                throw new ArgumentException("Попытка сравнения с " + obj.GetType().Name, nameof(obj));
+        }
+    }
+}
