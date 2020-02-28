7bbf792dc219ec9ab2f0b443522198ac4358d7c3
but-sergey <but-sergey@bk.ru>
2020-02-28 20:20:15 +0300

    Пример работы с делегатами

--- AsteroidGame/Students/Program.cs
+++ AsteroidGame/Students/Program.cs
@@ -47,10 +47,10 @@ namespace Students
 
             //var result = student.CompareTo(dekanat);
 
-            foreach (var std in dekanat2)
-            {
-                Console.WriteLine(std);
-            }
+            //foreach (var std in dekanat2)
+            //{
+            //    Console.WriteLine(std);
+            //}
 
             var average_rating = dekanat2.Average(s => s.AverageRating);
             var sum_average_rating = dekanat2.Sum(s => s.AverageRating);
@@ -59,7 +59,76 @@ namespace Students
 
             var random_rating = rnd.NextValue(2, 3, 4, 5);
 
+            //StudentProcessor processor = new StudentProcessor(GetIndexedStudentName);
+            //StudentProcessor processor = GetIndexedStudentName;
+
+            //var index = 0;
+            //foreach (var s in dekanat2)
+            //    Console.WriteLine(processor(s, index++));
+
+            //Console.ReadLine();
+            //processor = GetAverageStudentRating;
+            //index = 0;
+            //foreach (var s in dekanat2)
+            //    Console.WriteLine(processor(s, index++));
+
+            //Console.ReadLine();
+            //ProcessStudents(dekanat2, GetIndexedStudentName);
+
+            //Console.ReadLine();
+            //ProcessStudents(dekanat2, GetAverageStudentRating);
+
+            //ProcessStudentStandard(dekanat2, PrintStudent);
+
+            //var metric = GetStudentMetrics(dekanat2, std => std.Name.Length + (int)(student.AverageRating * 10));
+
             Console.ReadLine();
         }
+
+        private static void PrintStudent(Student student)
+        {
+            Console.WriteLine("Студент: {0}", student.Name);
+        }
+
+        public static void ProcessStudentStandard(IEnumerable<Student> Students, Action<Student> action)
+        {
+            foreach (var s in Students)
+            {
+                action(s);
+            }
+        }
+
+        public static int[] GetStudentMetrics(IEnumerable<Student> Students, Func<Student, int> GetMetric)
+        {
+            var result = new List<int>();
+
+            foreach (var student in Students)
+            {
+                result.Add(GetMetric(student));
+            }
+
+            return result.ToArray();
+        }
+
+        public static void ProcessStudents(IEnumerable<Student> Students, StudentProcessor Processor)
+        {
+            var index = 0;
+            foreach (var s in Students)
+                Console.WriteLine(Processor(s, index + 1));
+        }
+
+        private static string GetIndexedStudentName(Student student, int Index)
+        {
+            return $"{student.Name}[{Index}]";
+        }
+
+        public static string GetAverageStudentRating(Student student, int Index)
+        {
+            return $"[{Index}]:{student.Name} - {student.AverageRating}";
+        }
+
+
     }
+
+    internal delegate string StudentProcessor(Student Student, int Index);
 }
